a9528ca1d3be346da3c879a2e13d03b1bf92fbc4
usc <claus-alexander.usener@viadee.de>
2016-09-23 21:25:46 +0200

    provide serialVersionUID for NamedExceptions

--- CamundaStaticValidator/src/main/java/de/viadee/bpm/camundaStaticValidator/config/reader/ConfigReaderException.java
+++ CamundaStaticValidator/src/main/java/de/viadee/bpm/camundaStaticValidator/config/reader/ConfigReaderException.java
@@ -2,6 +2,8 @@ package de.viadee.bpm.camundaStaticValidator.config.reader;
 
 public class ConfigReaderException extends Exception {
 
+  private static final long serialVersionUID = 7310325755339963999L;
+
   public ConfigReaderException(final Throwable e) {
     super(e);
   }
--- CamundaStaticValidator/src/main/java/de/viadee/bpm/camundaStaticValidator/processing/ConfigItemNotFoundException.java
+++ CamundaStaticValidator/src/main/java/de/viadee/bpm/camundaStaticValidator/processing/ConfigItemNotFoundException.java
@@ -2,6 +2,8 @@ package de.viadee.bpm.camundaStaticValidator.processing;
 
 public class ConfigItemNotFoundException extends Exception {
 
+  private static final long serialVersionUID = 3363611391920321684L;
+
   public ConfigItemNotFoundException(final String message) {
     super(message);
   }
