86242a672eaff30b8919aedf585219153cbaf19b
Marcel Flasskamp <Marcel.Flasskamp@viadee.de>
2017-07-14 09:48:11 +0200

    run without springcontext

--- viadeeProcessApplicationValidator/src/main/java/de/viadee/bpm/vPAV/ProcessApplicationValidator.java
+++ viadeeProcessApplicationValidator/src/main/java/de/viadee/bpm/vPAV/ProcessApplicationValidator.java
@@ -6,13 +6,32 @@ import de.viadee.bpm.vPAV.beans.BeanMappingGenerator;
 
 public class ProcessApplicationValidator extends AbstractRunner {
 
+    /*
+     * run vPAV with given ApplicationContext (Spring)
+     * 
+     * @param ApplicationContext
+     */
     public static boolean assertBPMModelConsistency(ApplicationContext ctx) {
-        
+
         BeanMappingGenerator.generateBeanMappingFile(ctx);
         retrieveClassLoader();
         run_vPAV();
-        
-        if(AbstractRunner.getfilteredIssues().isEmpty()){
+
+        if (AbstractRunner.getfilteredIssues().isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     * run vPAV (no Spring)
+     */
+    public static boolean assertBPMModelConsistency() {
+
+        retrieveClassLoader();
+        run_vPAV();
+
+        if (AbstractRunner.getfilteredIssues().isEmpty()) {
             return true;
         }
         return false;
