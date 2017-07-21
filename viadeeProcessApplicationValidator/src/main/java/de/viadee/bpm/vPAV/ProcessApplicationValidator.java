ed246aba889ea04b8e8449eaca6df6b756349fdc
b41 <b41@NBW7W311.intern.viadee.de>
2017-07-21 12:37:35 +0200

    Changed return type from bool to collection

--- viadeeProcessApplicationValidator/src/main/java/de/viadee/bpm/vPAV/ProcessApplicationValidator.java
+++ viadeeProcessApplicationValidator/src/main/java/de/viadee/bpm/vPAV/ProcessApplicationValidator.java
@@ -1,26 +1,26 @@
 package de.viadee.bpm.vPAV;
 
+import java.util.Collection;
+
 import org.springframework.context.ApplicationContext;
 
 import de.viadee.bpm.vPAV.beans.BeanMappingGenerator;
+import de.viadee.bpm.vPAV.processing.model.data.CheckerIssue;
 
 public class ProcessApplicationValidator extends AbstractRunner {
 
     /*
      * run vPAV with given ApplicationContext (Spring)
-     * 
+     *
      * @param ApplicationContext
      */
-    public static boolean assertBPMModelConsistency(ApplicationContext ctx) {
+    public static Collection<CheckerIssue> assertBPMModelConsistency(ApplicationContext ctx) {
 
         beanMapping = BeanMappingGenerator.generateBeanMappingFile(ctx);
         retrieveClassLoader();
         run_vPAV();
 
-        if (AbstractRunner.getfilteredIssues().isEmpty()) {
-            return true;
-        }
-        return false;
+        return AbstractRunner.getfilteredIssues();
     }
 
     /*
