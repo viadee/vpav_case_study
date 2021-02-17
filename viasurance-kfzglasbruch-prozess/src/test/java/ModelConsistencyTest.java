3def0f0e7a3cc01a09750252d4a1ce3073853859
Sascha Di Bernardo <sascha.dibernardo@viadee.de>
2021-02-17 12:01:35 +0100

    Added metrics for case study

--- viasurance-kfzglasbruch-prozess/src/test/java/ModelConsistencyTest.java
+++ viasurance-kfzglasbruch-prozess/src/test/java/ModelConsistencyTest.java
@@ -1,9 +1,4 @@
-
-import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.Collection;
-
+import de.viadee.bpm.vPAV.ProcessApplicationValidator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import de.viadee.bpm.vPAV.ProcessApplicationValidator;
-import de.viadee.bpm.vPAV.processing.model.data.CheckerIssue;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { SpringTestConfig.class })
@@ -26,6 +20,5 @@ public class ModelConsistencyTest {
         assertTrue("Model inconsistency found. Please check target folder for validation output",
                 ProcessApplicationValidator.findModelInconsistencies(ctx).isEmpty());
     }
-
 
 }
